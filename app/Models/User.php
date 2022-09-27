dd147c57d19a66aaa572dcfeeca603e816250864
robertdennis <robertdennispersonal@gmail.com>
2022-09-27 16:43:27 -0400

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+namespace App\Models;
+
 use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -13,17 +14,6 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable, HasUlids;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
 
     /**
      * The attributes that should be hidden for serialization.
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
+        //     'name' => 'Test App\Models\User',
         //     'email' => 'test@example.com',
         // ]);
     }
