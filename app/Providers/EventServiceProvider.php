c21d38a29300f99f87eb964d340223641b14753d
robertdennis <robertdennispersonal@gmail.com>
2023-01-01 16:57:59 -0500

    wip migration of forums outside of core

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
+use Despawn\Support\Providers\Steam\SteamExtendSocialite;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -20,7 +20,7 @@ class EventServiceProvider extends ServiceProvider
         ],
         \SocialiteProviders\Manager\SocialiteWasCalled::class => [
             // ... other providers
-            \SocialiteProviders\Steam\SteamExtendSocialite::class.'@handle',
+            SteamExtendSocialite::class.'@handle',
         ],
     ];
 
